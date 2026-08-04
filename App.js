c574c9ef2890acdee8d40c9104cff9340df32081
EnricoSpin <easpinardi@gmail.com>
2026-08-04 16:56:18 -0300

    Lição: Fazer get da api

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Sistema de Cadastro</Text>
     </View>
   );
 }
+
+//https://jsonplaceholder.typicode.com/users
+//get na api
 
 const styles = StyleSheet.create({
   container: {
